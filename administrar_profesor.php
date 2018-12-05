b312b03db836d82377e72d09e9a47eb5a7bcac2a
EliasCesar <eliasdiaz.cesar@gmail.com>
2018-12-05 17:31:55 -0600

    Agregando Crud profesores

---
+++ administrar_profesor.php
@@ -0,0 +1,19 @@
+<?php
+require_once("DbAcad.php");
+	if (isset($_POST['insertar'])) {
+		$sql = "CALL sp_ingresarProfesor($_POST['nombre'], $_POST['nombre2'], $_POST['apellido'], $_POST['apellido2'], $_POST['correo'], $_POST['fecha'], $_POST['sexo'], $_POST['direccion'], $_POST['numero'], $_POST['telefono'], $_POST['estado'])";
+				$result = $conn->query($sql);
+		header('Location: alumnos.php');
+
+	}elseif(isset($_POST['actualizar'])){
+		
+		header('Location: alumnos.php');
+
+	}elseif ($_GET['accion']=='e') {
+		
+		header('Location: alumnos.php');		
+
+	}elseif($_GET['accion']=='a'){
+		header('Location: actualizar_alumnos.php');
+	}
+?>
---
+++ profesores.php
@@ -0,0 +1,40 @@
+<?php include 'header.php'; 
+require_once("DbAcad.php");
+?>
+
+<!--work collections start-->
+	<div id="work-collections">
+		<div class="container">
+		<table class="highlight">			
+			<tr>
+				<td><a href="ingresoprofesor.php">Ingresar</a></td>
+			</tr>
+			<tr>
+				<td><a href="actualizar_profesor.php">Actualizar</a></td>
+			</tr>
+		</table>
+		  <table class="highlight">
+			<thead>
+			  <tr>
+				  <th>Id</th>
+				  <th>Nombre</th>
+				  <th>Operaciones</th>
+			  </tr>
+			</thead>
+			<tbody>
+			<?php 
+				$sql = "SELECT a.id_alumno as 'Carnet', CONCAT(p.primero_nombre, ' ', p.segundo_nombre, ' ',  p.primer_apellido, ' ', p.segundo_apellido)as 'Nombres' FROM alumno a Inner Join persona p;";
+				$result = $conn->query($sql);
+
+				if ($result->num_rows > 0) {
+					while($row = $result->fetch_assoc()) {
+						echo "<tr><td>" . $row["Carnet"]. " </td><td>" . $row["Nombres"]. "<td> <td><a href=\"administrar_alumnos.php?id=" . $row["Carnet"]. "&accion=a\">Actualizar</a> - <a href=\"administrar_alumnos.php?id=" . $row["Carnet"]. "&accion=e\">Eliminar</a>   </td></tr>";
+					}
+				}
+				?>
+			</tbody>
+		  </table>
+		</div>
+	</div>
+<!--work collections end-->
+<?php include 'footer.php'; ?>
